60e73044a0144e2c3f7f1f6510d348f9eed1e78d
Stuart Preston <stuart@pendrica.com>
2016-11-02 04:57:00 -0700

    Fix test to be positive

--- examples/resource_group.rb
+++ examples/resource_group.rb
@@ -8,7 +8,7 @@ require 'chef/provisioning/azurerm'
 with_driver 'AzureRM:b6e7eee9-e4cd-41ae-8432-03ab624df016'
 
 azure_resource_group 'chef-provisioning_examples_resource_group' do
-  location 'Westjj Europe'
+  location 'West Europe'
   tags hello: 'world'
   action :create
 end
